262e904abb32b4fa0f37ec59a2cc4671bd6f9e31
Adityagupta1625 <guptaaditya2512@gmail.com>
2021-09-01 09:24:08 +0530

    intial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import Navbar from "./components/Navbar";
+import About from "./components/About";
+import TextForm from "./components/TextForm";
+import { useState } from "react";
+import Alert from "./components/Alert";
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom"
 function App() {
+  const [darkMode, setDarkMode] = useState("light");
+  const [alert, setalert] = useState(null);
+
+  const showAlert = (message, type) => {
+    setalert({
+      msg: message,
+      type: type,
+    });
+  };
+  setTimeout(() => {
+    showAlert(null, null);
+  }, 4000);
+
+  const toggleMode = () => {
+    if (darkMode === "light") {
+      setDarkMode("dark");
+      showAlert("Dark Mode has been enabled", "success");
+      document.body.style.backgroundColor = "#042743";
+      // Document.title="TextUtils-dark";
+     
+    } 
+    else {
+      setDarkMode("light");
+      showAlert("Light Mode has been enabled", "success");
+      document.body.style.backgroundColor = "white";
+
+      // just for reference: 
+      // setInterval(() => {
+      //   Document.title="it is amazing";
+      // }, 2000);
+      // setInterval(() => {
+      //   Document.title="it is must install";
+      // }, 2000);
+      // Document.title="TextUtils-light";
+    }
+   
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Router>
+      <Navbar
+        title="Textutils"
+        mode={darkMode}
+        toggleMode={toggleMode}
+      ></Navbar>
+      <Alert alert={alert}></Alert>
+      <Switch>
+        <Route exact path="/">
+        
+        <TextForm
+            heading="Enter the text to analyze"
+            mode={darkMode}
+            showAlert={showAlert}
+          ></TextForm>
+        </Route>
+        <Route exact path="/about" >
+        <About mode={darkMode}/>
+          
+        </Route>
+      </Switch>
+      
+    </Router>
+    </>
   );
 }
 
---
+++ src/components/TextForm.js
@@ -0,0 +1,99 @@
+import React, { useState } from "react";
+export default function TextForm(props) {
+  const [text, setText] = useState("");
+  // text="new text" wrong way to change state
+  const handleUpClick = () => {
+    let newText = text.toUpperCase();
+    setText(newText);
+    props.showAlert("Converted to UpperCase", "success");
+  };
+  const handledownClick = () => {
+    let newText = text.toLowerCase();
+    setText(newText);
+    props.showAlert("Converted to LowerCase", "success");
+  };
+  const handleclearText = () => {
+    let newText = "";
+    setText(newText);
+    props.showAlert("Cleared Text", "success");
+  };
+  const handlePunc = () => {
+    let punctuation = '!"#$%&()*+,:[;<=>?@^_`{|}~]/';
+    let rawLetters = text.split("");
+    var cleanLetters = rawLetters.filter(function (letter) {
+      return punctuation.indexOf(letter) === -1;
+    });
+    let newText = cleanLetters.join("");
+    setText(newText);
+    props.showAlert("Removed Punctutations", "success");
+  };
+  const handleCopy = () => {
+    var t = document.getElementById("myBox");
+    t.select();
+    navigator.clipboard.writeText(t.value);
+    props.showAlert("Copied Text", "success");
+  };
+  const handleRemoveExtraSpaces = () => {
+    let rawLetters = text.split("  ");
+    let newText = rawLetters.join("");
+    setText(newText);
+    props.showAlert("Removed ExtraSpaces", "success");
+  };
+  const handleOnChange = (event) => {
+    setText(event.target.value);
+  };
+
+  return (
+    <>
+      <div
+        className="container "
+        style={{ color: props.mode === "dark" ? "white" : "black" }}
+      >
+        <h1 className="my-2">{props.heading}</h1>
+        <div className="mb-3">
+          <textarea
+            className="form-control"
+            id="myBox"
+            value={text}
+            onChange={handleOnChange}
+            rows="8"
+            style={{
+              backgroundColor: props.mode === "dark" ? "#042743" : "white",
+              color: props.mode === "dark" ? "white" : "black",
+            }}
+          ></textarea>
+        </div>
+        <button className="btn btn-primary mx-1 my-1" onClick={handleUpClick}disabled={text.length===0} >
+          Convert to Uppercase
+        </button>
+        <button className="btn btn-primary mx-1 my-1" onClick={handledownClick}disabled={text.length===0} >
+          Convert to Lowercase
+        </button>
+        <button className="btn btn-primary mx-1 my-1" onClick={handleclearText}disabled={text.length===0} >
+          ClearText
+        </button>
+        <button className="btn btn-primary mx-1 my-1" onClick={handlePunc}disabled={text.length===0} >
+          Remove Punctution
+        </button>
+        <button className="btn btn-primary mx-1  my-1" onClick={handleRemoveExtraSpaces}disabled={text.length===0} >
+          RemoveExtraSpaces
+        </button>
+        <button className="btn btn-primary mx-1 my-1" onClick={handleCopy}disabled={text.length===0} >
+          Copy Text
+        </button>
+      </div>
+      <div
+        className="container my-3"
+        style={{ color: props.mode === "dark" ? "white" : "black" }}
+      >
+        <h1>Your text Summary</h1>
+        <p>
+          {text.split(/\s/).filter((element)=>{return element.length!==0}).length} words and {text.length} characters
+        </p>
+        <p>{0.008 * text.split(/\s/).filter((element)=>{return element.length!==0}).length} Minutes it will take to read</p>
+        <h3>Preview</h3>
+        <p>{text.length > 0 ? text : "Enter something to preview here"}</p>
+      </div>
+    </>
+  );
+}
